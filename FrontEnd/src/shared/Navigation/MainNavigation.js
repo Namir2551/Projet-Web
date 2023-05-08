05c52a1c86929c1d0fedf4ef2d5a0968149a7bd8
NK2551 <namirkas22@gmail.com>
2023-05-08 14:43:50 -0400

    img

--- FrontEnd/src/shared/Navigation/MainNavigation.js
+++ FrontEnd/src/shared/Navigation/MainNavigation.js
@@ -35,9 +35,13 @@ function MainNavigation(props) {
           <span />
           <span />
         </button>
-        <h1 className="main-navigation__title">
-          <Link to="/">La gestion d'inscription</Link>
-        </h1>
+        
+          <Link to="/"> 
+          <img src="https://www.cmontmorency.qc.ca/wp-content/uploads/2018/03/Logomo_1400.png" 
+                alt="Collège MontMorency">
+            </img>
+          </Link>
+        
         <nav className="main-navigation__header-nav">
           <NavLinks />
         </nav>
