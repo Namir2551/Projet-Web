7348d0dac4696fb1ce1717168821070cb227e3ab
Guillaume Giguère <2185208@cmontmorency.qc.ca>
2023-05-15 16:56:31 -0400

    Rajout backend

--- FrontEnd/src/etudiant/pages/ajouterEtudiant.js
+++ FrontEnd/src/etudiant/pages/ajouterEtudiant.js
@@ -1,33 +1,89 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useHttpClient } from "../../shared/hooks/http-hook";
 import "./ajouterEtudiant.css";
 
-function AjouterEtudiant() {
+const AjouterEtudiant = () => {
+    const {error, sendRequest, clearError } = useHttpClient();
+    const [formState, inputHandler] = useState(
+        {
+          numDA: {
+            value: '',
+            isValid: false
+          },
+          nomEtudiant: {
+            value: '',
+            isValid: false
+          },
+          courrielEtudiant: {
+            value: '',
+            isValid: false
+          },
+          profilSortieEtudiant: {
+            value: '',
+            isValid: false
+          }
+        },
+        false
+      );
+
+      const etudiantSubmitHandler  = async event =>  {
+        event.preventDefault();
+        console.log(formState.inputs);
+    
+        try {
+          const reponseData = await sendRequest(
+            "http://localhost:5000/api/etudiants/ajouterEtudiant",
+            "POST",
+            JSON.stringify({
+              numDA: formState.inputs.numDA.value,
+              nomEtudiant: formState.inputs.nomEtudiant.value,
+              courrielEtudiant: formState.inputs.courrielEtudiant.value,
+              profilSortieEtudiant: formState.inputs.profilSortieEtudiant.value,
+            }),
+            {
+              "Content-Type": "application/json",
+            }
+          );
+          console.log(reponseData);
+        } catch (err) {
+          console.log(err);
+        }
+      };
+
     return (
         <div className="nouveau-etudiant">
-        <form>
+        <form onSubmit={etudiantSubmitHandler}>
             <div className='nouveau-etudiant__controls'>
                 <div className='nouveau-etudiant__control'>
                     <label>Numero d'admission</label>
                     <input
+                        id="numDA"
                         type='text'
+                        onInput={inputHandler}
                     />
                 </div>
                 <div className='nouveau-etudiant__control'>
                     <label>Nom</label>
                     <input
+                        id="nom"
                         type='text'
+                        onInput={inputHandler}
                     />
                 </div>
                 <div className='nouveau-etudiant__control'>
                     <label>Courriel</label>
                     <input
+                        id="courriel"
                         type='text'
+                        onInput={inputHandler}
                     />
                 </div>
                 <div className='nouveau-etudiant__control'>
                     <label>Profil de sortie</label>
                     <input
+                        id="profilSortie"
                         type='text'
+                        onInput={inputHandler}
                     />
                 </div>
             </div>
--- FrontEnd/src/etudiant/pages/listeEtudiant.js
+++ FrontEnd/src/etudiant/pages/listeEtudiant.js
@@ -2,9 +2,10 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import "./listeEtudiant.css";
 import ListeEtudiant from "../components/ListeEtudiant";
+import { useHttpClient } from "../../shared/hooks/http-hook";
 
 const ListeEtudiantPage = () => {
-    const [loadedEtudiants, setLoadedEtudiants] = useState([
+    const [loadedEtudiant, setLoadedEtudiant] = useState([
         {
             id: "1",
             numDA: "123",
@@ -13,10 +14,26 @@ const ListeEtudiantPage = () => {
             profilSortieEtudiant: "D",
           },
     ]);
+    const {error, sendRequest, clearError } = useHttpClient();
+    const [loadedEtudiants, setLoadedEtudiants] = useState();
+
+    useEffect(() => {
+        const fetchEtudiants = async () => {
+          try {
+            const responseData = await sendRequest(
+              `http://localhost:5000/api/etudiants/listeEtudiant`
+            );
+            setLoadedEtudiants(responseData.etudiants);
+          } catch (err) {
+
+          }
+        };
+        fetchEtudiants();
+      }, [sendRequest]);
 
     return (
         <React.Fragment>
-            <ListeEtudiant items={loadedEtudiants} />
+            {loadedEtudiants && <ListeEtudiant items={loadedEtudiants} />}
         </React.Fragment>
     );
   }
